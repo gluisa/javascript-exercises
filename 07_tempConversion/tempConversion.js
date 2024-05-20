5715a01a3009ee8c1569932a2108474929b235e1
gluisa <76462853+gluisa@users.noreply.github.com>
2024-05-20 09:56:52 -0400

    Assigment 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrStart, celsEnd) {
+  celsEnd = (fahrStart - 32) * 5/9;
+  celsEndNum = Number(celsEnd.toFixed(1));
+  return celsEndNum;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsStart, fahrEnd) {
+  fahrEnd = celsStart * 9/5 + 32;
+  fahrEndNum = Number(fahrEnd.toFixed(1));
+  return fahrEndNum;
 };
 
 // Do not edit below this line
